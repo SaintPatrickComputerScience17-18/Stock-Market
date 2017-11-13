378021e2da2a8727f18a3247d141d42442c28e8e
Mr. Fleming <efleming@stpatrick.org>
2017-11-13 08:27:19 -0600

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,26 @@
+function Portfolio(){
+    //instance variable
+    this.stocks;
+
+
+    //helper function
+    function findStock(trk,list){
+        //i represents the index of the trk you are looking for in this.stocks
+        let i;
+        for(){
+            if(){
+
+            }
+        }
+        return i;
+    }
+
+    //instance functions
+    this.add = function(stk){};
+    this.update = function(tkr,val){};
+    this.buy = function(tkr,qnt){};
+    this.sell = function(tkr,qnt){};
+    this.totalValue = function(){};
+}
+
+module.exports = Portfolio;
---
+++ stock.js
@@ -0,0 +1,13 @@
+function Stock(/*fill*/){
+
+    //Initialize the instance variables below
+
+
+    //declare and define totalValue function below
+
+
+    //declarea and define sell(q) function below
+    
+}
+
+module.exports = Stock;
